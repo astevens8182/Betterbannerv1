e61a6ab2e3db2fe2eaa1df11b1f008b24c87c081
astevens8182 <37556349+astevens8182@users.noreply.github.com>
2019-11-07 15:40:45 -0500

    Update db-init.js

--- src/db-init.js
+++ src/db-init.js
@@ -6,12 +6,12 @@ import "firebase/auth";
 var firebaseConfig = {
   /* content from your Firebase Dashboard */
   apiKey: "AIzaSyAjTJun732nLtAoGKcvTqjGXEzu6OysiDw",
-    authDomain: "betterbanner-f49d2.firebaseapp.com",
-    databaseURL: "https://betterbanner-f49d2.firebaseio.com",
-    projectId: "betterbanner-f49d2",
-    storageBucket: "betterbanner-f49d2.appspot.com",
-    messagingSenderId: "486277424142",
-    appId: "1:486277424142:web:3d2b00ae3b23dee34dc85f"
+  authDomain: "betterbanner-f49d2.firebaseapp.com",
+  databaseURL: "https://betterbanner-f49d2.firebaseio.com",
+  projectId: "betterbanner-f49d2",
+  storageBucket: "betterbanner-f49d2.appspot.com",
+  messagingSenderId: "486277424142",
+  appId: "1:486277424142:web:3d2b00ae3b23dee34dc85f"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
